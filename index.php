0eeb941a7443c053c19aba1f1fa096a669e9be40
Caio Paulino Costa <130261742+CaioPaulinoCosta@users.noreply.github.com>
2023-08-17 19:26:15 -0300

    Adicionando estrutura de pastas ao projeto.

--- index.php
+++ index.php
@@ -1,49 +1,8 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="short icon" href="img/logo.png" />
+<?php
+require_once("templates/Header.php")
+?>
 
-    <!-- CSS DO PROJETO -->
-    <link rel="stylesheet" href="css/styles.css">
-    <!-- BOOTSTRAP -->
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-4bw+/aepP/YC94hEpVNVgiZdgIC5+VKNBQNGCHeKRQN+PtmoHDEXuppvnDJzQIu9" crossorigin="anonymous">
-    <title>PawFolio</title>
-</head>
-<body>
-
-    <header>
-        <nav class="navbar navbar-expand-lg">
-            <div class="container">
-              <a class="navbar-brand" href="#"><img src="img/logo.png" alt="logo"></a>
-              <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-                <span class="navbar-toggler-icon"></span>
-              </button>
-              <div class="collapse navbar-collapse" id="navbarSupportedContent">
-                <ul class="navbar-nav m-auto mb-2 mb-lg-0">
-                  <li class="nav-item">
-                    <a class="nav-link" href="#">Home</a>
-                  </li>
-                  <li class="nav-item">
-                    <span class="nav-link">|</span>
-                  </li>
-                  <li class="nav-item">
-                    <a class="nav-link" href="#">Serviços</a>
-                  </li>
-                  <li class="nav-item">
-                    <span class="nav-link">|</span>
-                  </li>
-                  <li class="nav-item">
-                    <a class="nav-link" href="#">Agenda</a>
-                  </li>
-                </ul>
-              </div>
-            </div>
-          </nav>
-    </header>
-
-    <main>
+<main>
       <div class="container-fluid main-header">
         <div class="container p-5">
           <div class="main-header-content">
@@ -136,18 +95,6 @@
       </div>
     </main>
 
-    <footer>
-      <div class="container text-center p-5">
-        <span>Desenvolvido por <br>Caio Paulino Costa, Theo Vittor Tavora, João Pedro Pelogia e Pedro Fortunato<br></span>
-        <span>&copy;2023 PawFolio</span>
-      </div>
-    </footer>
-    
-
-
-</body>
-<!-- BOOTSTRAP -->
-<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-HwwvtgBNo3bZJJLYd8oVXjrBZt8cqVSpeBNS5n7C8IVInixGAoxmnlMuBnhbgrkm" crossorigin="anonymous"></script>
-<!-- FONTAWESOME -->
-<script src="https://kit.fontawesome.com/e4264bec92.js" crossorigin="anonymous"></script>
-</html>
+<?php
+  require_once("templates/Footer.php")
+?>
---
+++ templates/Footer.php
@@ -0,0 +1,20 @@
+<?php
+
+require_once("config/globals.php");
+require_once("config/db.php");
+
+?>
+
+<footer>
+      <div class="container text-center p-5">
+        <span>Desenvolvido por <br>Caio Paulino Costa, Theo Vittor Tavora, João Pedro Pelogia e Pedro Fortunato<br></span>
+        <span>&copy;2023 PawFolio</span>
+      </div>
+    </footer>
+    
+</body>
+<!-- BOOTSTRAP -->
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-HwwvtgBNo3bZJJLYd8oVXjrBZt8cqVSpeBNS5n7C8IVInixGAoxmnlMuBnhbgrkm" crossorigin="anonymous"></script>
+<!-- FONTAWESOME -->
+<script src="https://kit.fontawesome.com/e4264bec92.js" crossorigin="anonymous"></script>
+</html>
